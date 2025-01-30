77baccdde55be1bd7683c2c9978de08464dc77a8
mhsn <mhsn.bakhshi696@gmail.com>
2025-01-30 21:01:20 +0330

    add file

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const { Telegraf, Markup } = require( "telegraf");
+const { connectToDB }  = require( "./db");
+const { insertUser, getUserRole }  = require( "./utils/qurey");
+const { checkUserMembership }  = require( "./utils/actions");
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.use( async (ctx, next) => {
+  await insertUser(ctx) 
+
+  const isMemberJoined = await checkUserMembership(ctx);
+  if (isMemberJoined) {
+  return next()
+    } else {
+      ctx.reply(`سلام ${ctx.chat.first_name} عزیز،
+        برای استفاده از ربات میبایست داخل کانال تلگرام نود یونیک عضو باشید، سپس مجدد /start نمایید.`, Markup.inlineKeyboard([
+         [Markup.button.url("کانال نود یونیک", "https://t.me/NodeUnique")]
+        ]))
+ }
+  
+})
+
+bot.start(async (ctx) =>{
+ const {role} = await getUserRole(ctx)
+   ctx.reply(`سلام ${ctx.chat.first_name} عزیز. \n به ربات نود یونیک خوش اومدی یکی از گزینه های زیر رو انتخاب کن:`);  
+});
+
+
+bot.command("donit", (ctx) => {
+  ctx.reply(`${ctx.chat.first_name} عزیز، 
+برای حمایت از ربات میتوانید یکی از راه ها را انتخاب نمایید. حمایت شما باعث دلگرمی برنامه نویس ربات است.🙏❤️
+
+1) بوست کردن کانال تلگرام (کاربران پریموم)
+- https://t.me/boost/NodeUnique
+2) دونیت قهوه برای برنامه نویس 
+- https://www.coffeete.ir/MhsnBakhshi
+3) دادن 🌟 به ریپو گیت هاب ربات
+- https://github.com/MhsnBakhshi/NodeUniqueBot`)
+})
+
+
+connectToDB();
+console.log("Bot Running");
+bot.launch();
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
